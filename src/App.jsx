ed5b23902e5a001e2bbaca044977cabc2a9b1467
Himanshu Rathore <himanshu.rathore2561@gmail.com>
2023-03-19 13:06:20 +0530

    lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,27 @@
-import React from 'react'
-import { Routes, Route} from 'react-router-dom'
+import React, { lazy, Suspense } from 'react'
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import Nav from './components/Nav'
-import Home from './pages/Home'
-import HistoryTab from './pages/HistoryTab'
-import CreateBucket from './pages/CreateBucket'
-import CreateCard from './pages/CreateCard'
-import PageNotFound from './pages/PageNotFound'
+
+const Home = lazy(() => import('./pages/Home'))
+const HistoryTab = lazy(() => import('./pages/HistoryTab'))
+const CreateBucket = lazy(() => import('./pages/CreateBucket'))
+const CreateCard = lazy(() => import('./pages/CreateCard'))
+const PageNotFound = lazy(() => import('./pages/PageNotFound'))
 
 function App() {
-
   return (
     <>
       <Nav />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/history' element={<HistoryTab />} />
-        <Route path='/createbucket' element={<CreateBucket />} />
-        <Route path='/createcard' element={<CreateCard />} />
-        <Route path='/*' element={<PageNotFound />} />
-      </Routes>
+      <Suspense fallback={<h1>Please Wait, Loading...</h1>}>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/history' element={<HistoryTab />} />
+          <Route path='/createbucket' element={<CreateBucket />} />
+          <Route path='/createcard' element={<CreateCard />} />
+          <Route path='/*' element={<PageNotFound />} />
+        </Routes>
+      </Suspense>
     </>
   )
 }
